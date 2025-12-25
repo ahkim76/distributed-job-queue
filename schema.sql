4313245cdf7eee2df542333cd5699c859e4b4387
ahkim76 <ahkim9976@gmail.com>
2025-12-25 14:19:48 -0600

    Create initial table schema

---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE jobs (
+    id                  BIGSERIAL PRIMARY KEY,  -- unique job ID
+    queue_name          TEXT NOT NULL DEFAULT 'default', -- lets you support multiple queues (payments, emails, etc)
+    job_type            TEXT NOT NULL,          -- which handler/function runs this job
+    payload JSONB       NOT NULL,               -- JSON data the job needs
+
+    status              TEXT NOT NULL,          -- job lifecycle: pending | processing | done | dead
+    attempts            INT NOT NULL DEFAULT 0, -- how many times job was tried
+    max_attempts        INT NOT NULL DEFAULT 5, -- retry cap
+    priority            INT NOT NULL DEFAULT 0, 
+
+    visible_at          TIMESTAMPZ NOT NULL DEFAULT NOW(), -- future-ready: delayed jobs + retry backoff
+    lease_expires_at    TIMESTAMPZ,             -- crash safety: if worker dies, another can reclaim
+    last_error          TEXT,                   -- debugging and DLQ info
+
+    created_at          TIMESTAMPZ NOT NULL DEFAULT NOW(),
+    updated_at          TIMESTAMPZ NOT NULL DEFAULT NOW()
+)
